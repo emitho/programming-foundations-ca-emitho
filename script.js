9f6742371c4fd0e8b2363a6cc426d578993b4ca5
emitho <emil.thorsplass@gmail.com>
2023-01-22 19:09:59 -0300

    Add object macbook

---
+++ script.js
@@ -0,0 +1,25 @@
+const macbook = {
+  madeBy: "Apple",
+  year: 2017,
+  modelInProduction: false,
+  specs: [
+    memory = { 
+      gb: 8,
+      type: "lpddr3",
+    },
+    chipset = {
+      processor: "1.2 GHz Dual-Core Intel Core m3",
+      graphics: "Intel HD Graphics 615 ",
+    },
+    misc = {
+      os: "macOS",
+      version: 13.1,
+      serialNumber: "nOt@r3@1$3r1@lnumb3r",
+    }
+  ],
+  doesItsJob: true,
+  isSluggish: true,
+  openAppMax: 22,
+  yearsUntilReplaced: 1,
+}
+
